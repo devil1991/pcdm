c14e19c30091253e279a6b163e6d13cc041b4827
andou <antonio.pastorino@gmail.com>
2013-09-16 15:48:32 +0200

    Modifica sul template della press.

--- wp-content/themes/pcdm_theme/archive-pcdm_press.php
+++ wp-content/themes/pcdm_theme/archive-pcdm_press.php
@@ -38,7 +38,7 @@ get_header();
       <?php if(isset($press_block[PcdmPress::TYPE_PREFIX . 'pdf_file'])):?>
       <a href="<?php echo $press_block[PcdmPress::TYPE_PREFIX . 'pdf_file'] ?>" target="_blank">
       <?php endif;?>
-      <img src="<?php echo $img[0] ?>" alt="">
+      <img src="<?php echo $press_block['pcdm_press_wall_image'] ?>" alt="">
       <?php if(isset($press_block[PcdmPress::TYPE_PREFIX . 'pdf_file'])):?>
       </a>
       <?php endif;?>

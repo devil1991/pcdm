98b04c9739747d3ddd72cca6251332a4ecbe5c0d
andou <antonio.pastorino@gmail.com>
2013-09-17 09:10:39 +0200

    Aggiunta store list nel menu header in attesa delle icons.

--- wp-content/themes/pcdm_theme/header.php
+++ wp-content/themes/pcdm_theme/header.php
@@ -74,6 +74,7 @@ if (isset($polylang)){
               <li><a href="http://www.paulacademartori.tumblr.com/" target="_blank" title=""><?php _e('Diary')?></a></li>
               <li><a href="<?php echo get_post_type_archive_link( PcdmNews::TYPE_IDENTIFIER )?>" title=""><?php _e('News')?></a></li>
               <li><a href="<?php echo get_post_type_archive_link(PcdmShoponline::TYPE_IDENTIFIER )?>" title=""><?php _e('shop online')?></a></li>
+              <li><a href="<?php echo get_post_type_archive_link(PcdmStore::TYPE_IDENTIFIER )?>" title=""><?php echo _e('store list')?></a></li>
             </ul>
           </nav>
           <nav class="choose-language">
@@ -159,6 +160,7 @@ if (isset($polylang)){
                                     <li><a href="http://www.paulacademartori.tumblr.com/" target="_blank" title=""><?php echo _e('Diary')?></a></li>
                                     <li><a href="<?php echo get_post_type_archive_link( PcdmNews::TYPE_IDENTIFIER )?>" title=""><?php echo _e('News')?></a></li>
                                     <li><a href="<?php echo get_post_type_archive_link(PcdmShoponline::TYPE_IDENTIFIER )?>" title=""><?php echo _e('shop online')?></a></li>
+                                    <li><a href="<?php echo get_post_type_archive_link(PcdmStore::TYPE_IDENTIFIER )?>" title=""><?php echo _e('store list')?></a></li>
                                 </ul>
                             </nav>
                             <nav class="nav-social">
